e7d6d4e2de21d0265b829d9b4e1d665a26a0d28c
tobiasriccone <tobi_riccone@hotmail.com>
2024-08-18 17:40:44 -0300

    Agrega test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -19,3 +19,21 @@ test('El usuario inicia sesion', async ({ page }) => {
 
   await expect(homePage.logo).toBeVisible()
 });
+
+test('El usuario inicia sesion 2', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+  const homePage = new HomePage(page)
+
+  await page.goto('https://saucedemo.com/');
+
+  await page.route(
+      "**/*.{png,jpg}",
+      (route) => route.abort()
+  )
+
+  await loginPage.usernameInput.fill("standard_user")
+  await loginPage.passwordInput.fill("secret_sauce")
+  await loginPage.loginButton.click()
+
+  await expect(homePage.logo).toBeVisible()
+});
